24d46729bff54f0ac3d519b3547b7751253cef47
Marceulos <marcel.luan.05@hotmail.com>
2025-07-06 23:25:15 -0300

    Add files via upload

---
+++ biblioteca.h
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+struct Jogador {
+    int* dinheiro;
+    string* simbolos;
+    int num_simbolos;
+    int* vitorias;
+    int* derrotas;
+    int* valor_ganho;
+    int* valor_perdido;
+};
+
+void inicializarJogo(Jogador* jogador, int dinheiro_inicial = 100);
+void finalizarJogo(Jogador* jogador);
+void mostrarDinheiro(Jogador* jogador);
+void mostrarEstatisticas(Jogador* jogador);
+int sortearSimbolo(Jogador* jogador);
+bool jogar(Jogador* jogador, int aposta);
+void salvarDados(Jogador* jogador);
+void carregarDados(Jogador* jogador);
+
+// PRA FAZER: adicionar mais umas função, tipo uma pra exibir as regras
+// ex: void mostrarRegras();
